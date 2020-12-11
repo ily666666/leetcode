81cdff6e4068ba2a3ffe78f3ba407140fd1f08a6
bubble <867296114@qq.com>
2020-12-11 22:52:20 +0800

    '修改main函数'

--- 0283-Move-Zeroes/main2.cpp
+++ 0283-Move-Zeroes/main2.cpp
@@ -25,8 +25,14 @@ public:
 
 
 int main() {
-    std::cout << "Hello, 擦破World!" << std::endl;
 
+    int arr[]={0,1,0,3,12};
+    vector<int> vec(arr, arr+sizeof(arr)/sizeof(int));
+    Solution().moveZeroes(vec);
+
+    for (int i=0;i<vec.size();i++)
+        cout<<vec[i]<<" ";
+    cout<<endl;
     return 0;
 }
 
